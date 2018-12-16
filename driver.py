424e8b13b98f81d9d73a6f67cf09a1134c3e6540
AnnMarie <40plusalive@gmail.com>
2018-12-16 15:57:08 -0500

    lab done

---
+++ driver.py
@@ -0,0 +1,8 @@
+class Driver:
+    
+    def __init__(self, first, last, favorite_hobby):
+        self.first = first
+        self.last = last
+        self.favorite_hobby = favorite_hobby
+        
+        
---
+++ passenger.py
@@ -0,0 +1,9 @@
+
+class Passenger:
+    
+    def __init__(self, first, last, email, rides_taken):
+        self.first = first
+        self.last = last
+        self.email = email
+        self.rides_taken = rides_taken
+
